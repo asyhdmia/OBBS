3b60ed3b4d4a39fdfc91611a5d3fe41408f777b3
asyhdmia <nrasdmia@gmail.com>
2024-06-30 16:07:38 +0800

    process_login.php

--- TestProject/process_login.php
+++ TestProject/process_login.php
@@ -1,57 +1,179 @@
 <?php
-
 // Database connection details
 $servername = "localhost";
-$username = "your_username";
-$password = "your_password";
-$dbname = "testobbs"; 
+$username = "root"; // Update with your MySQL username
+$password = ""; // Update with your MySQL password if applicable
+$database = "testobbs";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$connection = new mysqli($servername, $username, $password, $database);
 
 // Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
 }
 
-// Handle form submission
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $username = $_POST["inputUsername"];
-  $password = $_POST["inputPassword"];
-  $role = $_POST["data[User][role]"]; // Get the role from the radio button
+$errorMessage = "";
+$successMessage = "";
 
-  // Prepare and execute SQL statement
-  $sql = "SELECT * FROM users_login WHERE username = ? AND password = ?";
-  $stmt = $conn->prepare($sql);
-  $stmt->bind_param("ss", $username, $password);
-  $stmt->execute();
-  $result = $stmt->get_result();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST["username"]; 
+    $password = $_POST["password"]; 
+    $role = isset($_POST["role"]) ? $_POST["role"] : 'donor'; 
+    $rememberMe = isset($_POST["rememberMe"]) ? 1 : 0; 
 
-  // Check if user exists
-  if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    if ($row["role"] == $role) { // Check if role matches
-      session_start();
-      $_SESSION["username"] = $username;
-      $_SESSION["role"] = $role;
-      // Redirect to the appropriate page based on the role
-      if ($role == 'admin') {
-        header("Location: admin_dashboard.php");
-        exit;
-      } else {
-        header("Location: donor_dashboard.php");
-        exit;
-      }
+    // Sanitize and validate user inputs
+    $username = mysqli_real_escape_string($connection, $username);
+    $password = mysqli_real_escape_string($connection, $password);
+
+    // Prepare and execute the SQL query
+    $sql = "SELECT * FROM users_login WHERE username = '$username' AND password = '$password'";
+    $result = $connection->query($sql);
+
+    if ($result->num_rows > 0) {
+        // User found, login successful
+        $successMessage = "Login successful!";
+        // Redirect to the appropriate page based on the user's role
+        // Example:
+        // header("Location: admin.php"); // For admin role
+        // header("Location: donor.php"); // For donor role
     } else {
-      echo "Invalid role";
+        // User not found, login failed
+        $errorMessage = "Invalid username or password.";
     }
-  } else {
-    echo "Invalid username or password";
-  }
 
-  $stmt->close();
+    // Close the database connection
+    $connection->close();
 }
-
-$conn->close();
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>User Login Form</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-danger">
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Login Into Your Account</h1>
+                                    </div>
+                                    <div class="tab-content">
+                                        <div class="tab-pane fade show active" id="obbs">
+                                            <form action="login.php" method="post">
+                                                <div class="form-group text-center">
+                                                <div class="form-group text-center"></div>
+                                                    <div class="form-check form-check-inline">
+                                                        <input class="form-check-input" type="radio" name="data[User][role]" id="donorCheckbox" value="donor" required="required" checked>
+                                                        <label class="form-check-label">
+                                                            Donor
+                                                        </label>
+                                                    </div>
+                                                    <div class="form-check form-check-inline">
+                                                        <input class="form-check-input" type="radio" name="data[User][role]" id="adminCheckbox" value="admin" required="required">
+                                                        <label class="form-check-label">
+                                                            Admin
+                                                        </label>
+                                                    </div>
+                                                </div>   
+                                                <hr>
+                                    <form class="user">
+                                        <?php if (!empty($errorMessage)): ?>
+                                            <div class="error"><?php echo $errorMessage; ?></div>
+                                        <?php endif; ?>
+                                        <?php if (!empty($successMessage)): ?>
+                                            <div class="success"><?php echo $successMessage; ?></div>
+                                        <?php endif; ?>
+                                        <div class="form-group">
+                                            <input type="username" class="form-control form-control-user"
+                                                id="inputUsername" aria-describedby="username"
+                                                placeholder="Username" name="username" value="<?php echo isset($username) ? $username : ''; ?>">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user"
+                                                id="inputPassword" placeholder="Password" name="password" value="<?php echo isset($password) ? $password : ''; ?>">
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="custom-control custom-checkbox small">
+                                                <input type="checkbox" class="custom-control-input" id="customCheck" name="rememberMe" <?php if(isset($rememberMe) && $rememberMe) echo "checked"; ?>>
+                                                <label class="custom-control-label" for="customCheck">Remember
+                                                    Me</label>
+                                            </div>
+                                        </div>
+                                        <a href="index.html" class="btn btn-primary btn-user btn-block">
+                                            Login
+                                        </a>
+                                        <hr>
+                                        <div class="signup-link">
+                                            <p>Don't have an account yet? <a href="donorsignup.html">Sign Up</a></p>
+                                        </div>
+                                        <hr>
+                                        <a href="index.html" class="btn btn-google btn-user btn-block">
+                                            <i class="fab fa-google fa-fw"></i> Login with Google
+                                        </a>
+                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                        </a>
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                    </div>
+                                    <div class="text-center">
+                                        <a class="small" href="donorsignup.html">Create an Account!</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
