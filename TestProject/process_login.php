ae9b3aeffa52af0ea74478279c311250e9d781ca
asyhdmia <nrasdmia@gmail.com>
2024-07-05 14:26:46 +0800

    update

--- TestProject/process_login.php
+++ TestProject/process_login.php
@@ -47,7 +47,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 $stmt->execute();
 
                 // Redirect to the donor page
-                header("Location: ..dashboard/OBBS/Project_obbs/sidebar-05/index.html"); // Redirect to the donor homepage
+                header("Location: http://localhost/dashboard/OBBS/Project_obbs/sidebar-05/index.html"); // Redirect to the donor homepage
                 exit();
             } else {
                 $errorMessage = "Invalid username or password.";
