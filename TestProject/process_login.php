3642caf61c960fd3293eb3e874fa59edfe56e5a3
asyhdmia <nrasdmia@gmail.com>
2024-07-07 18:09:47 +0800

    update process_login.php

--- TestProject/process_login.php
+++ TestProject/process_login.php
@@ -72,7 +72,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     $stmt->execute();
 
                     // Redirect to the admin page
-                    header("Location: http://localhost/dashboard/OBBS/administrator/adminpage.php"); // Replace with your admin page URL
+                    header("Location: http://localhost/dashboard/OBBS/administratorpage/donor-list.php"); // Replace with your admin page URL
                     exit();
                 } else {
                     $errorMessage = "Invalid username or password.";
