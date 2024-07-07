823e99889326ca0cadf3a49db37ca41cb2301195
asyhdmia <nrasdmia@gmail.com>
2024-07-07 18:39:15 +0800

    update

--- administratorpage/includes/leftbar.php
+++ administratorpage/includes/leftbar.php
@@ -2,17 +2,14 @@
 			<ul class="ts-sidebar-menu">
 
 				<li class="ts-label">Main</li>
-				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
 
 <li><a href="#"><i class="fa fa-files-o"></i> Blood Group</a>
 <ul>
-<li><a href="add-bloodgroup.php">Add Blood Group</a></li>
 <li><a href="manage-bloodgroup.php">Manage Blood Group</a></li>
 </ul>
 </li>
 <li><a href="donor-list.php"><i class="fa fa-users"></i> Donor List</a></li>
 <li><a href="blood-requests.php"><i class="fa fa-users"></i> Blood Requests</a></li>
-<li><a href="request-received-bydonar.php"><i class="fa fa-search"></i>Search Blood Request</a></li>
 
 			</ul>
 		</nav>
