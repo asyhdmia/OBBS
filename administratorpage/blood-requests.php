816c6c33398fd1bc419ccb27119a0791771cba04
asyhdmia <nrasdmia@gmail.com>
2024-07-08 16:16:39 +0800

    update

--- administratorpage/blood-requests.php
+++ administratorpage/blood-requests.php
@@ -35,7 +35,7 @@ blood-request.php
 	.table-wrapper {
 		min-width: 800px;
         background: #fff;
-        padding: 20px;        
+        padding: 50px;        
         box-shadow: 0 1px 1px rgba(0,0,0,.05);
     }
 	.table-title {
